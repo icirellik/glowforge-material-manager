f5992bb1240b56395f3aa718cdf4eb1add11645f
Cameron Rollheiser <icirellik@gmail.com>
2019-08-06 23:54:16 -0400

    QR Code Test
    Add a test to verify htat QR Codes are working.
    
    Add zxing with fallback.

---
+++ src/lib/qrCode.test.ts
@@ -0,0 +1,26 @@
+import { readQrCode, zx } from './qrCode';
+import path from 'path';
+
+const currentPath = path.dirname(__dirname);
+const IMAGE_PATH_CUSTOM_1 = path.join(currentPath, '..', 'test-images', 'Custom:3f77f07.jpg');
+const IMAGE_PATH_YO = path.join(currentPath, '..', 'test-images', 'frame.png');
+
+describe('QR Code test', () => {
+  it('detect code: Yo!', async () => {
+    const path2 = `file://${path.resolve(IMAGE_PATH_YO)}`;
+    const code = await readQrCode(path2);
+    expect(code).toEqual('Yo!');
+  }, 12000);
+
+  // it('detect code: Custom:3f77f07', async () => {
+  //   const path2 = `file://${path.resolve(IMAGE_PATH_CUSTOM_1)}`;
+  //   const code = await readQrCode(path2);
+  //   expect(code).toEqual('Custom:3f77f07');
+  // }, 32000);
+
+  it('detect code (zx): Custom:3f77f07', async () => {
+    const path2 = `file://${path.resolve(IMAGE_PATH_CUSTOM_1)}`;
+    const code = await zx(path2);
+    expect(code).toEqual('Custom:3f77f07');
+  }, 32000);
+});
--- src/lib/qrCode.ts
+++ src/lib/qrCode.ts
@@ -1,56 +1,111 @@
 import QRCode from 'qrcode';
-// import jsQR from 'jsqr';
+// import * as QrScanner from 'qr-scanner';
+import jsQR from 'jsqr';
+// import { reject } from 'q';
+import fs from 'fs';
+import { BrowserQRCodeReader } from '@zxing/library';
 
-// const QR_IMAGE_SCALES = [1, 0.75, 0.5, 0.25, 0.1];
+const QR_IMAGE_SCALES = [1, 0.9, 0.87, 0.75, 0.6, 0.5, 0.4, 0.25, 0.2, 0.1, 0.05];
+
+export async function zx(url: string) {
+  const codeReader = new BrowserQRCodeReader();
+  return codeReader
+    .decodeFromImage(undefined, url)
+      .then(result => result.getText());
+}
 
 /**
  * Takes an image url and converts it to an image data at a 0.25 scale.
  *
  * @param imageUrl The image url to load.
  */
-// async function urlToImageData(imageUrl: string, scale: number): Promise<ImageData> {
-//   return new Promise(resolve => {
-//     const canvas = document.createElement('canvas');
-//     const context = canvas.getContext('2d');
-//     const image = new Image();
+async function urlToImageData(imageUrl: string, scale: number): Promise<ImageData> {
+  return new Promise((resolve, reject) => {
+    try {
+      const canvas = document.createElement('canvas');
+      const image = new Image();
 
-//     image.addEventListener('load', () => {
-//       canvas.width = image.naturalWidth * scale;
-//       canvas.height = image.naturalHeight * scale;
-//       context!.drawImage(image, 0, 0, canvas.width, canvas.height);
-//       resolve(context!.getImageData(0, 0, canvas.width, canvas.height));
-//     }, false);
-//     image.src = imageUrl;
-//   });
-// }
+      image.addEventListener('load', () => {
+        try {
+          const width = image.naturalWidth * scale;
+          const height = image.naturalHeight * scale;
+
+          canvas.width = width;
+          canvas.height = height;
+
+          const context = canvas.getContext('2d', { alpha: false });
+          context!.imageSmoothingEnabled = false;
+
+          // Move registration point to the center of the canvas
+          // context!.translate(width / 2, height / 2);
+          // context!.rotate(45 * Math.PI / 180);// angle must be in radians
+          // Move registration point back to the top left corner of canvas
+          // context!.translate((-width) / 2, (-height) / 2);
+
+          context!.drawImage(image, 0, 0, canvas.width, canvas.height);
+          const imageData = context!.getImageData(0, 0, canvas.width, canvas.height);
+
+          // Write to disk, testing only
+          // var img = canvas.toDataURL();
+          // var data = img.replace(/^data:image\/\w+;base64,/, "");
+          // var buf = new Buffer(data, 'base64');
+          // fs.writeFileSync(`a-${scale}.png`, buf);
+
+          resolve(imageData);
+        } catch (e) {
+          reject(e);
+        }
+      }, false);
+      image.onerror = (err) => {
+        console.log(err)
+      };
+
+      image.src = imageUrl;
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
 
 /**
  * Try to read a QR code from an image.
  *
  * @param imageUrl  The image url to scan.
  */
-// export async function readQrCode(imageUrl: string): Promise<string> {
-//   return new Promise(async resolve => {
-//     const imageDataPromises = QR_IMAGE_SCALES.map(scale => {
-//       return Promise.resolve()
-//         .then(() => {
-//           return urlToImageData(imageUrl, scale);
-//         }).then(imageData => {
-//           return jsQR(imageData.data, imageData.width, imageData.height);
-//         });
-//     });
+export async function readQrCode(imageUrl: string): Promise<string | null> {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const imageDataPromises = QR_IMAGE_SCALES.map(scale => {
+        return Promise.resolve()
+          .then(() => {
+            return urlToImageData(imageUrl, scale);
+          }).then(imageData => {
+            return jsQR(imageData.data, imageData.width, imageData.height, {
+              inversionAttempts: 'attemptBoth',
+            });
+          }).catch(e => console.log(e));
+        });
 
-//     const resolvedCodes = await Promise.all(imageDataPromises);
+      const resolvedCodes = await Promise.all(imageDataPromises);
+      const code = resolvedCodes.reduce((prev, cur) => {
+        if (prev) {
+          return prev;
+        }
+        if (cur) {
+          return cur;
+        }
+        return null;
+      }, null);
 
-//     const code = resolvedCodes.reduce((prev, cur) => {
-//       return (prev !== null) ? prev : cur;
-//     }, null);
-
-//     if (code) {
-//       resolve(code.data);
-//     }
-//   });
-// }
+      if (code) {
+        resolve(code.data);
+      }
+      resolve(null);
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
 
 /**
  * Takes some text data and encodes it into a QR Code as a data uril.
